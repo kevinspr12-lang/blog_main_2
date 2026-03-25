3f83817d774a71b9b7c3f1e08c6784d8d23f6b2e
kevinspr12-lang <kevinspr12@gmail.com>
2026-03-25 16:21:28 -0700

    añadi el tutorial

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 """
 URL configuration for blog_main project.
 
+
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
@@ -15,11 +16,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import home, detail
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('blog/<int:id>/', detail, name='post_detail'),
-]
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls'))
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
